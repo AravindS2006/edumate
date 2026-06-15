69d5e0fff2b721d7f1a1153eef75a92413a6692b
Aravindselvan <150777603+AravindS2006@users.noreply.github.com>
2026-06-15 09:33:07 +0530

    Update next-sitemap.config.js

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://edumate-sairam.vercel.app',
+    siteUrl: 'https://edumate1-sairam.vercel.app',
     generateRobotsTxt: true,
     exclude: ['/dashboard', '/dashboard/*', '/api*'],
     robotsTxtOptions: {
